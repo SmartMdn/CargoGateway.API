f3d08457f769004c6be90baca466be0c77c6e065
Mamivilia123 <i.matyuninn@gmail.com>
2025-07-21 13:11:23 +0300

    feat: add caching

--- CargoGateway.Core/Interfaces/ISearchRepository.cs
+++ CargoGateway.Core/Interfaces/ISearchRepository.cs
@@ -5,4 +5,5 @@ namespace CargoGateway.Core.Interfaces;
 public interface ISearchRepository
 {
     Task AddSearchResultAsync(SearchEntity search);
+    Task<SearchEntity?> FindRecentSearchAsync(string from, string to, DateOnly date, TimeSpan maxAge);
 }
--- CargoGateway.Infrastructure/Persistence/Repositories/SearchRepository.cs
+++ CargoGateway.Infrastructure/Persistence/Repositories/SearchRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Cargo.Libraries.Logistics.Models.Entities;
 using CargoGateway.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace CargoGateway.Infrastructure.Persistence.Repositories;
 
@@ -9,5 +10,19 @@ public class SearchRepository(ApplicationDbContext db) : ISearchRepository
     {
         db.SearchEntities.Add(search);
         await db.SaveChangesAsync();
+    }
+
+    public async Task<SearchEntity?> FindRecentSearchAsync(string from, string to, DateOnly date, TimeSpan maxAge)
+    {
+        var cutoffTime = DateTime.UtcNow - maxAge;
+        
+        return await db.SearchEntities
+            .Include(s => s.Shipments)
+                .ThenInclude(sh => sh.Legs)
+            .FirstOrDefaultAsync(s => 
+                s.From == from && 
+                s.To == to && 
+                s.Date == date && 
+                s.CreatedAtUtc >= cutoffTime);
     }
 }
